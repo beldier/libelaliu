8043694cc25bb73a36462e717c6b4c8faa3187b2
beldier <root@localhost.localdomain>
2020-01-22 17:23:26 -0400

    Day 7

---
+++ uva/uhunt/src/firstchapter/easyproblems/EmoogleBalance12279.java
@@ -0,0 +1,89 @@
+package firstchapter.easyproblems;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class EmoogleBalance12279 {
+	
+	private void solve() throws IOException {
+		int n = -1;
+		int cases = 1;
+		while((n = readInteger())!= 0) {
+			int treats = 0 ;
+			while(n-->0) {
+				if(readInteger()>0)
+					treats++;
+				else
+					treats--;
+			}
+			wr.printf("Case %d: %d\n",cases++,treats);
+		}
+	}
+
+	public static void main(String[] args) {
+		EmoogleBalance12279 var = new EmoogleBalance12279();
+		var.run();
+	}
+
+	private BufferedReader scan;
+	private StringTokenizer tokenizer;
+	private PrintWriter wr;
+
+	public int readInteger() throws IOException {
+		return Integer.parseInt(read());
+	}
+
+	public long readLong() throws IOException {
+		return Long.parseLong(read());
+	}
+
+	public double readDouble() throws IOException {
+		return Double.parseDouble(read());
+	}
+
+	public String read() throws IOException {
+		String res = "";
+		if (tokenizer.hasMoreTokens()) {
+			res = tokenizer.nextToken();
+		} else {
+			String aux = scan.readLine();
+			//if(aux == null){
+			//	wr.close();
+			//	System.exit(0);
+			//}
+
+			tokenizer = new StringTokenizer(aux, " ");
+			res = tokenizer.nextToken();
+		}
+		return res;
+	}
+
+	public void run() {
+		try {
+			scan = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/utils/input")));
+			tokenizer = new StringTokenizer("", "");
+			//wr = new PrintWriter(new File("src/utils/output"));
+			wr = new PrintWriter(System.out);
+
+			solve();
+
+			scan.close();
+			wr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			wr.close();
+			System.exit(0);
+		}
+	}
+}
